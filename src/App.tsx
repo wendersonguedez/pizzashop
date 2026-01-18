ae9e0c0b2f57f5f30b7849339e1e1418edacd5e5
wenderson <wendersonguedes6@gmail.com>
2026-01-18 12:43:38 -0300

    chore(ui): setup shadcn/ui with tailwind and base configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export function App() {
 	return (
-		<>
-			<h1>Hello World</h1>
-		</>
+		<div className="flex justify-center items-center h-screen">
+			<Button>Clique</Button>
+		</div>
 	);
 }
