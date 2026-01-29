b96d1219f6796626995269d90c3e5cd9491fea3e
wenderson <wendersonguedes6@gmail.com>
2026-01-29 01:50:25 -0300

    feat(ui): implement generic pagination component

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/table";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./order-table-filters";
+import { Pagination } from "@/components/pagination";
 
 export function Orders() {
   return (
@@ -16,30 +17,31 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rouded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-16"></TableHead>
-                <TableHead className="w-35">Identificador</TableHead>
-                <TableHead className="w-35">Realizado há</TableHead>
-                <TableHead className="w-35">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-35">Total do pedido</TableHead>
-                <TableHead className="w-41"></TableHead>
-                <TableHead className="w-33"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, index) => {
-                return <OrderTableRow key={index} />;
-              })}
-            </TableBody>
-          </Table>
+          <div className="rouded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-16"></TableHead>
+                  <TableHead className="w-35">Identificador</TableHead>
+                  <TableHead className="w-35">Realizado há</TableHead>
+                  <TableHead className="w-35">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-35">Total do pedido</TableHead>
+                  <TableHead className="w-41"></TableHead>
+                  <TableHead className="w-33"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, index) => {
+                  return <OrderTableRow key={index} />;
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
