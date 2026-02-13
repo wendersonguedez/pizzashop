147e9e8a487b8dfe277e6ca26ed5430dae895572
wenderson <wendersonguedes6@gmail.com>
2026-02-13 12:36:44 -0300

    feat(dashboard): integrate revenue chart with api and date filter

---
+++ src/components/ui/date-range-picker.tsx
@@ -0,0 +1,63 @@
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import * as React from "react";
+import { type DateRange } from "react-day-picker";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface DateRangePickerProps extends React.HTMLAttributes<HTMLDivElement> {
+  date: DateRange | undefined;
+  onDateChange?: (date: DateRange | undefined) => void;
+}
+
+export function DateRangePicker({ date, onDateChange }: DateRangePickerProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <Label htmlFor="date-picker-range" className="mr-1 text-sm">
+        Período
+      </Label>
+
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            id="date-picker-range"
+            className="w-70 justify-start text-left font-normal"
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Selecione uma data</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="end">
+          {" "}
+          <Calendar
+            autoFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={onDateChange}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,13 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useMemo, useState } from "react";
+import type { DateRange } from "react-day-picker";
 import {
   CartesianGrid,
   Line,
   LineChart,
   ResponsiveContainer,
+  Tooltip,
   XAxis,
   YAxis,
 } from "recharts";
 import colors from "tailwindcss/colors";
 
+import { getDailyRevenueInPeriod } from "@/api/get-daily-revenue-in-period";
 import {
   Card,
   CardContent,
@@ -15,54 +21,107 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
-const data = [
-  { name: "Jan", amount: 2400 },
-  { name: "Fev", amount: 2210 },
-  { name: "Mar", amount: 2290 },
-  { name: "Abr", amount: 2000 },
-  { name: "Mai", amount: 2181 },
-  { name: "Jun", amount: 2500 },
-  { name: "Jul", amount: 2100 },
-];
+import { DateRangePicker } from "@/components/ui/date-range-picker";
 
 export function RevenueChart() {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
+  });
+
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryKey: ["metrics", "daily-revenue-in-period", dateRange],
+    queryFn: () =>
+      getDailyRevenueInPeriod({
+        from: dateRange?.from,
+        to: dateRange?.to,
+      }),
+  });
+
+  /**
+   * O endpoint retorna a receita em centavos, então aqui converto para reais dividindo por 100.
+   *
+   * O useMemo é usado para evitar fazer essa conversão toda vez que o componente renderizar,
+   * fazendo apenas quando os dados realmente mudarem.
+   */
+  const chartData = useMemo(() => {
+    if (!dailyRevenueInPeriod) return [];
+
+    return dailyRevenueInPeriod.map((item) => ({
+      ...item,
+      date: item.date,
+      receipt: item.receipt / 100,
+    }));
+  }, [dailyRevenueInPeriod]);
+
   return (
     <Card className="col-span-6">
-      <CardHeader className="flex-row items-center justify-between pb-8">
+      <CardHeader className="flex flex-row items-center justify-between pb-8">
         <div className="space-y-1">
           <CardTitle className="text-base font-medium">
             Receita no período
           </CardTitle>
           <CardDescription>Receita diária no período</CardDescription>
         </div>
+
+        <div className="flex items-center gap-3">
+          <DateRangePicker date={dateRange} onDateChange={setDateRange} />
+        </div>
       </CardHeader>
       <CardContent>
-        <ResponsiveContainer width="100%" height={240}>
-          <LineChart data={data} style={{ fontSize: 12 }}>
-            <YAxis
-              stroke="#888"
-              axisLine={false}
-              tickLine={false}
-              width={80}
-              tickFormatter={(value: number) =>
-                value.toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })
-              }
-            />
-            <XAxis dataKey="name" tickLine={false} axisLine={false} dy={16} />
+        {chartData && (
+          <ResponsiveContainer width="100%" height={240}>
+            <LineChart data={chartData} style={{ fontSize: 12 }}>
+              <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
 
-            <CartesianGrid vertical={false} className="stroke-muted" />
+              <YAxis
+                stroke="#888"
+                axisLine={false}
+                tickLine={false}
+                width={80}
+                tickFormatter={(value: number) =>
+                  value.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })
+                }
+              />
 
-            <Line
-              type="monotone"
-              dataKey="amount"
-              stroke={colors.violet[500]}
-            />
-          </LineChart>
-        </ResponsiveContainer>
+              <CartesianGrid vertical={false} className="stroke-muted" />
+
+              <Tooltip
+                cursor={false}
+                content={({ active, payload, label }) => {
+                  if (active && payload && payload.length) {
+                    return (
+                      <div className="border-border bg-card text-card-foreground rounded-lg border p-2 shadow-sm">
+                        <span className="text-muted-foreground block text-xs">
+                          {label}
+                        </span>
+                        <div className="flex items-center gap-2">
+                          <span className="dark:text-foreground text-foreground text-sm font-bold">
+                            {Number(payload[0].value).toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </span>
+                        </div>
+                      </div>
+                    );
+                  }
+                  return null;
+                }}
+              />
+
+              <Line
+                type="linear"
+                strokeWidth={2}
+                dataKey="receipt"
+                stroke={colors.violet[500]}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
       </CardContent>
     </Card>
   );
